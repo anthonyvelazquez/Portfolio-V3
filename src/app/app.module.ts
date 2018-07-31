0e29a1d10b5dddee20ecc71343f56b81e26bbf8c
Anthony <anthony@commoncents.tech>
2018-07-31 11:23:57 -0400

    Added Color Scheme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
-import {ProgressBarModule} from "angular-progress-bar"
+import {ProgressBarModule} from "angular-progress-bar";
+import { ScrollToModule } from '@nicky-lenaers/ngx-scroll-to';
+
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -17,7 +21,10 @@ import {ProgressBarModule} from "angular-progress-bar"
     BrowserModule,
     FlexLayoutModule,
     AngularFontAwesomeModule,
-    ProgressBarModule
+    ProgressBarModule,
+    FormsModule,
+    NgbModule.forRoot(),
+    ScrollToModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  FixedNavbar: boolean = false;
+  SaveNavbarPosition: number;
+  @ViewChild("navbar") NavBarElement;
+
   constructor() { }
 
   ngOnInit() {
+    this.SaveNavbarPosition = this.NavBarElement.nativeElement.offsetTop;
   }
+
+
+  @HostListener('window:scroll', ['$event']) 
+    scrollHandler(event) {
+      console.log("Window Top Position: ", window.pageYOffset)
+      console.log("Scroll Event - Nav Position: ", this.NavBarElement.nativeElement.offsetTop);
+      if(window.pageYOffset >= this.SaveNavbarPosition) {
+        this.FixedNavbar = true;
+      }
+      else {
+        this.FixedNavbar = false;
+      }
+    }
 
 }
